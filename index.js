8041395f5ab1b54d8ea8771a937e4402f22f1f08
Ajayi Davies <ajayidavies7@gmail.com>
2023-07-31 16:00:53 +0200

    added cors and static middleware

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ let data = [
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('build'));
 
 const morgan = require('morgan');
 
@@ -88,6 +91,7 @@ app.post('/api/persons', (request, response) => {
 });
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
